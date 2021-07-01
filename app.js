2a01830c0ebcc806b446ef873bb75685628cdf6d
Shouvik Mandal <shouvik0013@gmail.com>
2021-07-01 22:52:31 +0530

    added a 404 error page

--- app.js
+++ app.js
@@ -69,4 +69,8 @@ app.use(shopRoutes);
 
 // server.listen(3000);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
+
 app.listen(3000);   // this line does the same thing as previous two lines
