a08648192e24a40c8a2d257575e65d87d0b8ad5c
Shouvik Mandal <shouvik0013@gmail.com>
2021-07-05 22:03:17 +0530

    Some comments added in admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();    // this line returns a mini-express app
 // this mini express app is plugable means we can export it, use it anywhere
 
+// /admin/add-product
 router.get('/add-product', (req, res, next) => {
     // console.log('At add-product middleware!');
     res.send('<html><body><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form></body></html>');
@@ -14,7 +15,7 @@ router.get('/add-product', (req, res, next) => {
 //     res.redirect('/');
 // });
 
-
+// /admin/add-product
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);  // by default req(request) does not parse the incoming request body
     res.redirect('/');
