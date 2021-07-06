2a51e2f4e7974db21568159e23fb6a40f94deb85
Shouvik Mandal <shouvik0013@gmail.com>
2021-07-06 22:35:43 +0530

    rootDir added in admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
 const router = express.Router();    // this line returns a mini-express app
 // this mini express app is plugable means we can export it, use it anywhere
 
@@ -8,7 +9,7 @@ const router = express.Router();    // this line returns a mini-express app
 router.get('/add-product', (req, res, next) => {
     // console.log('At add-product middleware!');
     // res.send('<html><body><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form></body></html>');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 
